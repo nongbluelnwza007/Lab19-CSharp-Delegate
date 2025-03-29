d2ba0a8c424bd137e7976f107a7791dbded51772
nongbluelnwza007 <booblueboyy@gmail.com>
2025-03-29 22:05:30 +0700

    add

---
+++ Lab18_Ex03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public delegate void MyDelegate(string message);
+
+class InstanceMethod
+{
+    public void MethodA(string message)
+    {
+        Console.WriteLine($"You are calling instance MethodA() with message {message}");
+    }
+}
+
+static class StaticMethod
+{
+    public static void MethodB(string message)
+    {
+        Console.WriteLine($"You are calling static MethodB() with message {message}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var im = new InstanceMethod();
+        MyDelegate del = im.MethodA;
+        InvokeDelegate(del);
+
+        del = StaticMethod.MethodB;
+        InvokeDelegate(del);
+
+        del = (string message) => Console.WriteLine($"You are calling anonymous method with message {message}");
+        InvokeDelegate(del);
+    }
+
+    static void InvokeDelegate(MyDelegate myDel)
+    {
+        myDel("Hello World");
+    }
+}
---
+++ Lab18_Ex04/Program.cs
@@ -0,0 +1,34 @@
+﻿var im = new InstanceMethod();
+MyDelegate imdel = im.MethodA;
+MyDelegate smdel = StaticMethod.MethodB;
+MyDelegate amdel = (string message) => System.Console.WriteLine($"You are calling anonymous method with message {message}");
+System.Console.WriteLine("------------------");
+System.Console.WriteLine("imdel + smdel");
+MyDelegate del = imdel + smdel  ;
+del("Hello world");
+System.Console.WriteLine("------------------");
+System.Console.WriteLine("imdel + smdel + amdel");
+del  += amdel  ;
+del("Hello world");
+
+System.Console.WriteLine("------------------");
+System.Console.WriteLine("del -= imdel ");
+del -= imdel;
+del("Hello world");
+
+class InstanceMethod
+{
+     public void MethodA(string message)
+     {
+        System.Console.WriteLine($"You are calling instance MethodA() with message {message}");
+     }
+}
+static class StaticMethod
+{
+     public static void MethodB(string message)
+     {
+        System.Console.WriteLine($"You are calling static MethodB() with message {message}");
+     }
+}
+
+public delegate void MyDelegate(string message);
